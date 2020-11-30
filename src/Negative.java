2db9d362711dc3618464f5b0d836df8759a43b64
ahmetmenguc <ahmetmenguc98@gmail.com>
2020-11-30 23:08:35 +0300

    Negative class added.

---
+++ src/Negative.java
@@ -0,0 +1,12 @@
+public class Negative {
+    String name;
+    int cid;
+    int nid;
+
+    public Negative(String name, int cid, int nid) {
+        super();
+        this.name = name;
+        this.cid = cid;
+        this.nid = nid;
+    }
+}
