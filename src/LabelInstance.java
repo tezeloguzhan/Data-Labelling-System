51c1031c2aa6a4a32180f361103daaeb753d33b8
tezeloguzhan <tezeloguzhann@gmail.com>
2020-11-30 22:10:16 +0300

    LabelInstance class added

---
+++ src/LabelInstance.java
@@ -0,0 +1,14 @@
+public class LabelInstance {
+
+    LabelDefinition definition;
+    String target;
+    String value;
+
+    public LabelInstance(LabelDefinition definition, String target, String value) {
+        super();
+        this.definition = definition;
+        this.target = target;
+        this.value = value;
+    }
+
+}
