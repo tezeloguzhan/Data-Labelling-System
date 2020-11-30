a73e8afc9190157237edc01709df4a2221a8e249
fatihyuksell <61410293+fatihyuksell@users.noreply.github.com>
2020-11-30 22:55:10 +0300

    LabelVector class added.

---
+++ src/LabelVector.java
@@ -0,0 +1,12 @@
+public class LabelVector {
+
+    String name;
+    LabelDefinition[] labelDefinitions;
+
+    public LabelVector(String name, LabelDefinition[] labelDefinitions) {
+        super();
+        this.name = name;
+        this.labelDefinitions = labelDefinitions;
+    }
+
+}
