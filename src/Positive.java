a0f083e75477180a153d1f289ed91940f7302f3e
harunsari <harunsari2303@gmail.com>
2020-11-30 23:07:26 +0300

    Positive class added.

---
+++ src/Positive.java
@@ -0,0 +1,14 @@
+public class Positive {
+
+    String name;
+    int cid;
+    int nid;
+
+    public Positive(String name, int cid, int nid) {
+        super();
+        this.name = name;
+        this.cid = cid;
+        this.nid = nid;
+    }
+
+}
