5e0c7af75503b9921d1bffaa4ec56a281df9d7ef
tezeloguzhan <tezeloguzhann@gmail.com>
2020-12-05 01:19:00 +0300

    Main Class

---
+++ src/JSON_Reader.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public class JSON_Reader {
+
+
+
+}
