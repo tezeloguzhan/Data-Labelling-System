e96018c1cdbf299b846563c5aba2c44cc702717b
mumin-kocamaz <74527764+mumin-kocamaz@users.noreply.github.com>
2020-12-05 22:44:57 +0300

    assignUsersToAssignments method added

--- src/JSON.java
+++ src/JSON.java
@@ -83,6 +83,44 @@ public class JSON extends JSONArray {
         singleUserClassAssignment.put(key2, value2);
         return singleUserClassAssignment;
     }
+    public JSONObject assignUsersToAssignments(String filename) throws Exception {
 
+        JSONObject jsonObject = (JSONObject) readJson(filename);
+        JSONArray users = (JSONArray) jsonObject.get("users");
+        JSONArray classLabelAssignments = (JSONArray) jsonObject.get("class label assignments");
+
+        JSONObject outputJSON = new JSONObject();
+
+        JSONArray userClassAssignments = new JSONArray();
+
+        for (int i = 0; i < users.size(); i++) {
+
+            JSONObject user = (JSONObject) users.get(i);
+            ArrayList<Integer> allClassLabelIDs = new ArrayList<Integer>();
+
+            for (int j = 0; j < classLabelAssignments.size(); j++) {
+
+                JSONObject assignment = (JSONObject) classLabelAssignments.get(j);
+                JSONArray singleInstanceClassLabelIDs = (JSONArray) assignment.get("class label ids");
+
+                if (user.get("user id") == assignment.get("user id")) {
+
+                    for (int x = 0; x < singleInstanceClassLabelIDs.size(); x++) {
+                        Long longLabelID = (Long) singleInstanceClassLabelIDs.get(x);
+                        int labelID = longLabelID.intValue();
+                        if (!(allClassLabelIDs.contains(labelID))) {
+                            allClassLabelIDs.add(labelID);
+                        }
+                    }
+
+                }
+            }
+            userClassAssignments.add(createSingleUserClassAssignment("user id", user.get("user id"), "class label ids",
+                    allClassLabelIDs));
+        }
+        outputJSON.put("grouped class label assignments", userClassAssignments);
+
+        return outputJSON;
+    }
 
 }
