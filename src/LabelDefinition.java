81cb3819518451aa9838a6a50e334a6f0d16d49e
orhanfatih <bayazitorhan098@hotmail.com>
2020-12-05 20:24:39 +0300

    getId() and getName() added to LabelDefinition class

--- src/LabelDefinition.java
+++ src/LabelDefinition.java
@@ -1,10 +1,20 @@
+
 public class LabelDefinition {
 
 	String name;
+	Long id;
 
-	public LabelDefinition(String name) {
+	public LabelDefinition(Long id, String name) {
 		super();
+		this.id = id;
 		this.name = name;
 	}
 
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 }
