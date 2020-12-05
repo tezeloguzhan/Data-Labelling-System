80b6c5eb75f98fa867fc40904051073b5d31b27d
abbaskutay <abbaskutaymedine@gmail.com>
2020-12-05 20:20:22 +0300

    i did upadate user class

--- src/User.java
+++ src/User.java
@@ -1,7 +1,19 @@
 public class User {
-
+    Long id;
+    String name;
+    String type;
     String target;
     String value;
+
+    public User(Long id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
 
     public User(String target, String value) {
         super();
