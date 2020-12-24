8573681d044daa4fd37359b3a2cedc2a7c79f68f
orhanfatih <bayazitorhan098@hotmail.com>
2020-12-24 21:29:45 +0300

    User.json updated (consistency check probability)

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        String filename = "users.json";
+        String filename = "Users.json";
         JSONObject jsonObject = (JSONObject) JSON.readJson(filename);
         JSONArray users = (JSONArray) jsonObject.get("users");
         filename = "label dataset.json";
