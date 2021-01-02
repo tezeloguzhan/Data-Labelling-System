3d6ae85add2c56a65b7233204b50dd76a7007d89
tezeloguzhan <tezeloguzhann@gmail.com>
2021-01-02 22:42:45 +0300

    updated

--- src/com/company/RandomLabeler.java
+++ src/com/company/RandomLabeler.java
@@ -5,29 +5,18 @@ import java.util.Scanner;
 public class RandomLabeler extends DataLabeler{
 
 
-    public RandomLabeler(DataSet datasetInstance, com.company.User[] user) {
+    public RandomLabeler(DataSet datasetInstance, User user) {
         super(datasetInstance, user);
     }
 
     @Override
     public void labelData() {
-        System.out.println("Data Labeler Type: Random Labeler");
+
+        System.out.println("-----------Random Labeler-----------");
         DataInstance instanceList [] = this.datasetInstance.getInstance();
 
         ClassLabel labelList [] = this.datasetInstance.getClassLabel();
-        for(int i=0; i<this.user.length; i++){
-            System.out.printf("%d: %s\n",this.user[i].getId(), this.user[i].getUserName());
-        }
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Select User using the shown ID: ");
-        int userID = scan.nextInt();
-        com.company.User currUser = new com.company.User();
-        for(int i=0; i<this.user.length; i++){
-            if(user[i].getId() == userID){
-                currUser = user[i];
-                break;
-            }
-        }
+        User currUser = this.user;
         System.out.printf("Current User: ");
         System.out.println(currUser.getJsonObject().toJSONString());
         for(int i=0; i < instanceList.length; i++){
@@ -35,14 +24,12 @@ public class RandomLabeler extends DataLabeler{
             int rnd = new Random().nextInt(labelList.length);
             ClassLabel cl;
             cl = labelList[rnd];
-            com.company.User user = currUser;
+            User user = currUser;
             DataLabel dataLabel = new DataLabel(dataInst, user);
             dataLabel.addClassLabel(cl);
             this.dataLabelList[i] = dataLabel;
+            System.out.println(dataLabel.getJsonObject().toJSONString());
         }
-
-
-
 
     }
 }
